3dcba51064c1eddccebafa2035260d33946d81f2
Miguel <galleriesproductions@gmail.com>
2024-12-13 16:58:53 -0500

    rough draft routes

---
+++ src/routes/api/thoughtRoutes.ts
@@ -0,0 +1,30 @@
+import { Router } from 'express';
+import {
+    getAllThoughts,
+    getThoughtById,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction,
+} from '../../controllers/thoughtController';
+
+const router = Router();
+
+// /api/thoughts
+router.route('/')
+    .get(getAllThoughts)
+    .post(createThought);
+
+// /api/thoughts/:id
+router.route('/:id')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
+
+// /api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions')
+    .post(addReaction)
+    .delete(removeReaction);
+
+export default router;
