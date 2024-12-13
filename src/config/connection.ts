ca2e750fc9bcab208015d33c3f350c5366fe23ad
Miguel <galleriesproductions@gmail.com>
2024-12-13 16:43:33 -0500

    initial files

---
+++ src/config/connection.ts
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose';
+
+const connectionString = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialNetworkDB';
+
+mongoose.connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+export default mongoose.connection;
---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import db from './config/connection';
+import routes from './routes';
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server running on port ${PORT}!`);
+    });
+});
